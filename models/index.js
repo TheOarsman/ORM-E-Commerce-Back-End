33233d2a59b3870ca8a9784ba0309161c46e4401
TheOarsman <heinzulrichv@gmail.com>
2024-01-16 11:25:24 -0500

    index.js Associations made (hopefully correctly)

--- models/index.js
+++ models/index.js
@@ -1,16 +1,46 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
-// Categories have many Products
+Product.belongsTo(Category, {
+  through: {
+    foreignKey: "category_id",
+    unique: false,
+  },
+  // alias for the association
+  as: "versa_goods",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: "product_tag",
+    unique: false,
+  },
+  // alias for the association
+  as: "tag_blend",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: "tag_id",
+  },
+  // alias for the association
+  as: "products",
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  // alias for the association
+  as: "products",
+});
 
 module.exports = {
   Product,
