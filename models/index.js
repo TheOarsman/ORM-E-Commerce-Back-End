3830d42519613bb57e83c9cd82f1b68003f24b49
TheOarsman <heinzulrichv@gmail.com>
2024-01-16 11:27:13 -0500

    slight tweak to index.js associations

--- models/index.js
+++ models/index.js
@@ -6,31 +6,24 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  through: {
-    foreignKey: "category_id",
-    unique: false,
-  },
+  foreignKey: "category_id",
   // alias for the association
   as: "versa_goods",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    foreignKey: "product_tag",
-    unique: false,
-  },
+  through: ProductTag,
+  foreignKey: "product_tag",
+  unique: false,
   // alias for the association
   as: "tag_blend",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    foreignKey: "tag_id",
-  },
+  through: ProductTag,
+  foreignKey: "tag_id",
   // alias for the association
   as: "products",
 });
