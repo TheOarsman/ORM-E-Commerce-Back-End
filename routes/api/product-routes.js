fce124f80ab2578a3fa306f5753c2179f9ca453b
TheOarsman <heinzulrichv@gmail.com>
2024-01-18 18:19:15 -0500

    forgot to add "async" for last commit

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -120,7 +120,7 @@ router.put("/:id", async (req, res) => {
 });
 
 // delete one product by its `id` value
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const productTagIds = await Product.destroy({
       where: {
