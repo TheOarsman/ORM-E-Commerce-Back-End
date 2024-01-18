2affd2228bda7ce74f17b027bc863e40c71a7323
TheOarsman <heinzulrichv@gmail.com>
2024-01-18 18:18:00 -0500

    code added for deletion of Product by it's ID in 'product-routes.js'

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -119,8 +119,24 @@ router.put("/:id", async (req, res) => {
     });
 });
 
+// delete one product by its `id` value
 router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
+  try {
+    const productTagIds = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productTagIds) {
+      res.status(404).json({ message: "No product found with this ID!" });
+      return;
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (err) {
+    res.status(500).json;
+  }
 });
 
 module.exports = router;
