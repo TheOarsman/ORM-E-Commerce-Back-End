a44a20139029c05cb269d0346204b04c51d959cb
TheOarsman <heinzulrichv@gmail.com>
2024-01-22 07:51:59 -0500

    typo correction

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(rew.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [
         { model: Product, as: "products" },
         { model: Product, through: ProductTag, as: "tags" },
