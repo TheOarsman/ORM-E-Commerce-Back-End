1a96e6edf151f1bef45088680f6f8d1344787f90
TheOarsman <heinzulrichv@gmail.com>
2024-01-23 14:04:56 -0500

    err.message added to status(500)

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -79,7 +79,7 @@ router.delete("/:id", async (req, res) => {
 
     res.status(200).json({ message: "Category deleted successfully" });
   } catch (err) {
-    res.status(500).json;
+    res.status(500).json({ error: err.message });
   }
 });
 
